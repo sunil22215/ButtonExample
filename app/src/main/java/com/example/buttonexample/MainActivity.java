6d8f0dd8957fe404408d6100e07580d781dd6928
sunil <sunilkhedkar22215@gmail.com>
2020-11-11 20:57:12 +0530

    changes

--- app/src/main/java/com/example/buttonexample/MainActivity.java
+++ app/src/main/java/com/example/buttonexample/MainActivity.java
@@ -3,16 +3,40 @@ package com.example.buttonexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editText1,editText2;
     private Button button;
-
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+    public void addListenerOnButton(){
+
+        editText1=(EditText) findViewById(R.id.edittext1);
+        editText2=(EditText) findViewById(R.id.edittext2);
+        button=(Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value1=editText1.getText().toString();
+                String value2=editText2.getText().toString();
+                int a=Integer.parseInt(value1);
+                int b=Integer.parseInt(value2);
+
+                int sum=a+b;
+                Toast.makeText(getApplicationContext(),String.valueOf(sum),Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
