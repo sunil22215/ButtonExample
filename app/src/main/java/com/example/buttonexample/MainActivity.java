787b6a6a2111d47c9d193e92eefc4ed325c6f9af
sunil <sunilkhedkar22215@gmail.com>
2020-11-11 21:19:17 +0530

    after formated

--- app/src/main/java/com/example/buttonexample/MainActivity.java
+++ app/src/main/java/com/example/buttonexample/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.buttonexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
-    private EditText editText1,editText2;
+    private EditText editText1;
+    private EditText editText2;
     private Button button;
- 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,22 +20,22 @@ public class MainActivity extends AppCompatActivity {
 
         addListenerOnButton();
     }
-    public void addListenerOnButton(){
 
-        editText1=(EditText) findViewById(R.id.edittext1);
-        editText2=(EditText) findViewById(R.id.edittext2);
-        button=(Button) findViewById(R.id.button);
+    public void addListenerOnButton() {
+        editText1 = (EditText) findViewById(R.id.edittext1);
+        editText2 = (EditText) findViewById(R.id.edittext2);
+        button = (Button) findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String value1=editText1.getText().toString();
-                String value2=editText2.getText().toString();
-                int a=Integer.parseInt(value1);
-                int b=Integer.parseInt(value2);
+                String value1 = editText1.getText().toString();
+                String value2 = editText2.getText().toString();
+                int a = Integer.parseInt(value1);
+                int b = Integer.parseInt(value2);
 
-                int sum=a+b;
-                Toast.makeText(getApplicationContext(),String.valueOf(sum),Toast.LENGTH_SHORT).show();
+                int sum = a + b;
+                Toast.makeText(getApplicationContext(), String.valueOf(sum), Toast.LENGTH_SHORT).show();
 
             }
         });
